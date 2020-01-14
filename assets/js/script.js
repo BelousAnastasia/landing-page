0b36c1d5a4c5b4826f704168a28a4d2be63e4c2a
Anastasiia <ruslanovna.nry@gmail.com>
2020-01-14 23:32:28 +0200

    change all files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,77 @@
-$(document).ready(function () {
-	$(window).scroll(function(){
-		var scroll_img = $('.new_responsive_design').offset().top;
-		if ($(this).scrollTop() > scroll_img){
-			$('.animate').addClass('animated-left');
-		} else {
-			$('.animate').removeClass('animated-left');
-		}
-	});
-});
+
+// navigation
+
+var navTrigger = $('.nav-trigger'),
+    body = $('body');
+
+navTrigger.on('click', toggleNavigation);
+
+function toggleNavigation(event) {
+  event.preventDefault();
+  body.toggleClass('nav-open');
+}
+
+// cursor
+
+(function () {
+  'use strict';
+
+  var boxElem = document.getElementById('body');
+  var pointerElem = document.getElementById('particles-cursor');
+
+  function onMouseMove(event) {
+      var mouseX = event.pageX;
+      var mouseY = event.pageY;
+      var crd = boxElem.getBoundingClientRect();
+      var activePointer = crd.left <= mouseX && mouseX <= crd.right && crd.top <= mouseY && mouseY <= crd.bottom;
+
+      requestAnimationFrame(function movePointer() {
+          if (activePointer) {
+              pointerElem.style.left = Math.floor(mouseX - 100) + 'px';
+              pointerElem.style.top = Math.floor(mouseY - 50) + 'px';
+          } 
+      });
+  }
+
+  boxElem.addEventListener('mousemove', onMouseMove, false);
+
+})();
+
+
+// slider_img
+var items = $('.itemAboutus');
+var itemCount = items.length;
+var nextItem = $('.next');
+var previousItem = $('.previous');
+let count = 0;
+
+function showNextItem() {
+  items[count].classList.remove('active');
+
+  if(count < itemCount - 1) {
+    count++;
+  } else {
+    count = 0;
+  }
+
+  items[count].classList.add('active');
+  // console.log(count);
+}
+
+function showPreviousItem() {
+  items[count].classList.remove('active');
+
+  if(count > 0) {
+    count--;
+  } else {
+    count = itemCount - 1;
+  }
+
+  items[count].classList.add('active');
+  // console.log(count);
+}
+
+nextItem.on('click', showNextItem);
+previousItem.on('click', showPreviousItem);
+
+// map
